c6d38cdd5212a32d351ad9d6d5e8bd56b21d1028
Ashish Kamble <149313948+ASHISHx021@users.noreply.github.com>
2025-01-03 19:00:36 +0530

    credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,6 @@
-TWITTER_USERNAME = "Shubham_m2104"
-TWITTER_PASSWORD = "Shubzz@21"
+TWITTER_USERNAME = "your_username" #replace with your Twitter username
+
+TWITTER_PASSWORD = "your_password"#replace with your Twitter password 
 MONGODB_URI = "mongodb://localhost:27017/"
 PROXYMESH_USERNAME = "Ashish21"
 PROXYMESH_PASSWORD = "IGxASHISH21"
